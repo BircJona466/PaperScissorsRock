70f704856c103082319974915321afdcd64de85e
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-10-07 14:06:44 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace PaperScissorsRock
+{
+    partial class PaperScissorsRock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIntro = new System.Windows.Forms.Label();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblEnemy = new System.Windows.Forms.Label();
+            this.picEnemy = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblBetting = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.numBetting = new System.Windows.Forms.NumericUpDown();
+            this.lblHowMuch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBetting)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIntro
+            // 
+            this.lblIntro.AutoSize = true;
+            this.lblIntro.BackColor = System.Drawing.Color.Transparent;
+            this.lblIntro.Font = new System.Drawing.Font("Lucida Handwriting", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblIntro.Location = new System.Drawing.Point(114, 25);
+            this.lblIntro.Name = "lblIntro";
+            this.lblIntro.Size = new System.Drawing.Size(582, 41);
+            this.lblIntro.TabIndex = 0;
+            this.lblIntro.Text = "Welcome to Rock Paper Scissors!";
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackColor = System.Drawing.Color.White;
+            this.btnRock.BackgroundImage = global::PaperScissorsRock.Properties.Resources.rock;
+            this.btnRock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRock.Font = new System.Drawing.Font("Lucida Handwriting", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnRock.Location = new System.Drawing.Point(102, 119);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(151, 116);
+            this.btnRock.TabIndex = 1;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = false;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackgroundImage = global::PaperScissorsRock.Properties.Resources.paper;
+            this.btnPaper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPaper.Font = new System.Drawing.Font("Lucida Handwriting", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPaper.Location = new System.Drawing.Point(326, 119);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(151, 116);
+            this.btnPaper.TabIndex = 2;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.BackgroundImage = global::PaperScissorsRock.Properties.Resources.scissors;
+            this.btnScissors.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnScissors.Font = new System.Drawing.Font("Lucida Handwriting", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissors.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnScissors.Location = new System.Drawing.Point(557, 119);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(151, 116);
+            this.btnScissors.TabIndex = 3;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblResult.Location = new System.Drawing.Point(343, 66);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 27);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::PaperScissorsRock.Properties.Resources.table;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(293, 462);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 155);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblEnemy
+            // 
+            this.lblEnemy.AutoSize = true;
+            this.lblEnemy.BackColor = System.Drawing.Color.Transparent;
+            this.lblEnemy.Font = new System.Drawing.Font("Lucida Handwriting", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnemy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblEnemy.Location = new System.Drawing.Point(287, 252);
+            this.lblEnemy.Name = "lblEnemy";
+            this.lblEnemy.Size = new System.Drawing.Size(224, 31);
+            this.lblEnemy.TabIndex = 6;
+            this.lblEnemy.Text = "Enemy Choice...";
+            // 
+            // picEnemy
+            // 
+            this.picEnemy.BackColor = System.Drawing.Color.White;
+            this.picEnemy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picEnemy.Location = new System.Drawing.Point(320, 355);
+            this.picEnemy.Name = "picEnemy";
+            this.picEnemy.Size = new System.Drawing.Size(157, 101);
+            this.picEnemy.TabIndex = 7;
+            this.picEnemy.TabStop = false;
+            this.picEnemy.Visible = false;
+            this.picEnemy.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnExit.Location = new System.Drawing.Point(526, 407);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(225, 100);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblBetting
+            // 
+            this.lblBetting.AutoSize = true;
+            this.lblBetting.BackColor = System.Drawing.Color.Transparent;
+            this.lblBetting.Font = new System.Drawing.Font("Lucida Handwriting", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBetting.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblBetting.Location = new System.Drawing.Point(116, 306);
+            this.lblBetting.Name = "lblBetting";
+            this.lblBetting.Size = new System.Drawing.Size(99, 27);
+            this.lblBetting.TabIndex = 10;
+            this.lblBetting.Text = "Betting";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblScore.Location = new System.Drawing.Point(98, 344);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(0, 20);
+            this.lblScore.TabIndex = 11;
+            // 
+            // numBetting
+            // 
+            this.numBetting.Location = new System.Drawing.Point(247, 387);
+            this.numBetting.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numBetting.Name = "numBetting";
+            this.numBetting.Size = new System.Drawing.Size(45, 20);
+            this.numBetting.TabIndex = 12;
+            this.numBetting.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // lblHowMuch
+            // 
+            this.lblHowMuch.AutoSize = true;
+            this.lblHowMuch.BackColor = System.Drawing.Color.Transparent;
+            this.lblHowMuch.CausesValidation = false;
+            this.lblHowMuch.Font = new System.Drawing.Font("Lucida Handwriting", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHowMuch.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblHowMuch.Location = new System.Drawing.Point(31, 389);
+            this.lblHowMuch.Name = "lblHowMuch";
+            this.lblHowMuch.Size = new System.Drawing.Size(210, 15);
+            this.lblHowMuch.TabIndex = 13;
+            this.lblHowMuch.Text = "How much would you like to bet?";
+            // 
+            // PaperScissorsRock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PaperScissorsRock.Properties.Resources.lined_paper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(778, 551);
+            this.Controls.Add(this.lblHowMuch);
+            this.Controls.Add(this.numBetting);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblBetting);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.picEnemy);
+            this.Controls.Add(this.lblEnemy);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.lblIntro);
+            this.Name = "PaperScissorsRock";
+            this.Text = "Rock, Paper, Scissors!";
+            this.Load += new System.EventHandler(this.PaperScissorsRock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBetting)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIntro;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblEnemy;
+        private System.Windows.Forms.PictureBox picEnemy;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblBetting;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.NumericUpDown numBetting;
+        private System.Windows.Forms.Label lblHowMuch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PaperScissorsRock
+{
+    public partial class PaperScissorsRock : Form
+    {
+        Random generator = new Random();
+        int result;
+        int score;
+        
+        public PaperScissorsRock()
+        {
+            InitializeComponent();
+        }
+
+        private void PaperScissorsRock_Load(object sender, EventArgs e)
+        {
+            score = 10;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            lblScore.Text = ("You have " + score + " points..");
+            numBetting.Maximum = score;
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        { 
+            result = generator.Next(0, 3);
+
+            if (result == 1)
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.rock;
+                lblResult.Text = "Tie!";
+                numBetting.Maximum = score;
+            }
+            else if (result == 2)
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.paper;
+                lblResult.Text = "You Lose!";
+                score = (score - (int)numBetting.Value);
+                lblScore.Text = ("You have " + score + " points..");
+                numBetting.Maximum = score;
+            }
+            else
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.scissors;
+                lblResult.Text = "You Win!";
+                score = (score + ((int)numBetting.Value * 2));
+                lblScore.Text = ("You have " + score + " points..");
+                numBetting.Maximum = score;
+            }
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            result = generator.Next(0, 3);
+
+            if (result == 1)
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.rock;
+                lblResult.Text = "You Win!";
+                score = (score + ((int)numBetting.Value * 2));
+                lblScore.Text = ("You have " + score + " points..");
+                numBetting.Maximum = score;
+            }
+            else if (result == 2)
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.paper;
+                lblResult.Text = "Tie!";
+                numBetting.Maximum = score;
+            }
+            else
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.scissors;
+                lblResult.Text = "You Lose!";
+                score = (score - (int)numBetting.Value);
+                lblScore.Text = ("You have " + score + " points..");
+                numBetting.Maximum = score;
+            }
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            result = generator.Next(0, 3);
+
+            if (result == 1)
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.rock;
+                lblResult.Text = "You Lose!";
+                score = (score - (int)numBetting.Value);
+                lblScore.Text = ("You have " + score + " points..");
+                numBetting.Maximum = score;
+            }
+            else if (result == 2)
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.paper;
+                lblResult.Text = "You Win!";
+                score = (score + ((int)numBetting.Value * 2));
+                lblScore.Text = ("You have " + score + " points..");
+                numBetting.Maximum = score;
+            }
+            else
+            {
+                picEnemy.Visible = true;
+                picEnemy.BackgroundImage = Properties.Resources.scissors;
+                lblResult.Text = "Tie!";
+                numBetting.Maximum = score;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+        
+        
+            
+        }
+    
